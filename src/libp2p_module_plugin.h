8b9fc5deddcec9fd48c245a4f1ebde66d9a8c43d
Gabriel Cruz <8129788+gmelodie@users.noreply.github.com>
2026-02-18 16:38:02 -0300

    feat: stream functions (#15)

--- src/libp2p_module_plugin.h
+++ src/libp2p_module_plugin.h
@@ -4,6 +4,8 @@
 #include <QtCore/QVariant>
 #include <QtCore/QByteArray>
 #include <QtCore/QString>
+#include <QtCore/QHash>
+#include <QtCore/QReadWriteLock>
 
 #include <logos_api.h>
 #include <logos_api_client.h>
@@ -45,12 +47,30 @@ public:
     Q_INVOKABLE QString connectedPeers(int direction = 0) override;
     Q_INVOKABLE QString dial(const QString &peerId, const QString &proto) override;
 
+    /* ----------- Streams ----------- */
+    Q_INVOKABLE QString streamReadExactly(uint64_t streamId, size_t len) override;
+    Q_INVOKABLE QString streamReadLp(uint64_t streamId, size_t maxSize) override;
+    Q_INVOKABLE QString streamWrite(uint64_t streamId, const QByteArray &data) override;
+    Q_INVOKABLE QString streamWriteLp(uint64_t streamId, const QByteArray &data) override;
+    Q_INVOKABLE QString streamClose(uint64_t streamId) override;
+    Q_INVOKABLE QString streamCloseEOF(uint64_t streamId) override;
+    Q_INVOKABLE QString streamRelease(uint64_t streamId) override;
+
+    /* ----------- Sync Streams ----------- */
+    Q_INVOKABLE QByteArray syncStreamReadExactly(uint64_t streamId, size_t len) override;
+    Q_INVOKABLE QByteArray syncStreamReadLp(uint64_t streamId, size_t maxSize) override;
+    Q_INVOKABLE bool       syncStreamWrite(uint64_t streamId, const QByteArray &data) override;
+    Q_INVOKABLE bool       syncStreamWriteLp(uint64_t streamId, const QByteArray &data) override;
+    Q_INVOKABLE bool       syncStreamClose(uint64_t streamId) override;
+    Q_INVOKABLE bool       syncStreamCloseEOF(uint64_t streamId) override;
+    Q_INVOKABLE bool       syncStreamRelease(uint64_t streamId) override;
+
     /* ----------- Sync Connectivity ----------- */
     Q_INVOKABLE bool            syncConnectPeer(const QString &peerId, const QList<QString> multiaddrs, int64_t timeoutMs = -1) override;
     Q_INVOKABLE bool            syncDisconnectPeer(const QString &peerId) override;
     Q_INVOKABLE PeerInfo        syncPeerInfo() override;
     Q_INVOKABLE QList<QString>  syncConnectedPeers(int direction = 0) override;
-    Q_INVOKABLE QVariant        syncDial(const QString &peerId, const QString &proto) override;
+    Q_INVOKABLE uint64_t        syncDial(const QString &peerId, const QString &proto) override;
 
     /* ----------- Kademlia ----------- */
     Q_INVOKABLE QString toCid(const QByteArray &key) override;
@@ -107,7 +127,16 @@ private:
 
     libp2p_ctx_t *ctx = nullptr;
     libp2p_config_t config = {};
-    QString lastCaller; // for logging
+
+    QHash<uint64_t, libp2p_stream_t*> m_streams;
+    mutable QReadWriteLock m_streamsLock;
+    std::atomic<uint64_t> m_nextStreamId {1};
+
+    /* ----------- Stream Registry ----------- */
+    uint64_t addStream(libp2p_stream_t *stream);
+    libp2p_stream_t* getStream(uint64_t id) const;
+    libp2p_stream_t* removeStream(uint64_t id);
+    bool hasStream(uint64_t id) const;
 
     static void libp2pCallback(
         int callerRet,
@@ -162,7 +191,7 @@ private:
 
     static void connectionCallback(
         int callerRet,
-        libp2p_stream_t *conn,
+        libp2p_stream_t *stream,
         const char *msg,
         size_t len,
         void *userData
--- tests/sync.cpp
+++ tests/sync.cpp
@@ -6,9 +6,11 @@ class TestLibp2pModuleSync : public QObject
     Q_OBJECT
 
 private slots:
+
     /* ---------------------------
-     * Sync Connectivity tests
+     * Connectivity
      * --------------------------- */
+
     void testSyncConnectDisconnectPeer()
     {
         Libp2pModulePlugin plugin;
@@ -54,13 +56,113 @@ private slots:
 
         QString proto = "/test/1.0.0";
 
-        QCOMPARE(plugin.syncDial(fakePeer, proto), QVariant{});
+        QCOMPARE(plugin.syncDial(fakePeer, proto), 0);
 
         QVERIFY(plugin.syncLibp2pStop());
     }
 
     /* ---------------------------
-     * Sync Kademlia tests
+     * Stream
+     * --------------------------- */
+
+    void testSyncStreamClose()
+    {
+        Libp2pModulePlugin plugin;
+        QVERIFY(plugin.syncLibp2pStart());
+
+        uint64_t fakeStreamId = 1234;
+
+        // cannot close inexistent stream
+        QVERIFY(!plugin.syncStreamClose(fakeStreamId));
+
+        QVERIFY(plugin.syncLibp2pStop());
+    }
+
+    void testSyncStreamCloseEOF()
+    {
+        Libp2pModulePlugin plugin;
+        QVERIFY(plugin.syncLibp2pStart());
+
+        uint64_t fakeStreamId = 1234;
+
+        // cannot closeEOF inexistent stream
+        QVERIFY(!plugin.syncStreamCloseEOF(fakeStreamId));
+
+        QVERIFY(plugin.syncLibp2pStop());
+    }
+
+    void testSyncStreamRelease()
+    {
+        Libp2pModulePlugin plugin;
+        QVERIFY(plugin.syncLibp2pStart());
+
+        uint64_t fakeStreamId = 1234;
+
+        // cannot release inexistent stream
+        QVERIFY(!plugin.syncStreamRelease(fakeStreamId));
+
+        QVERIFY(plugin.syncLibp2pStop());
+    }
+
+    void testSyncStreamReadExactly()
+    {
+        Libp2pModulePlugin plugin;
+        QVERIFY(plugin.syncLibp2pStart());
+
+        uint64_t fakeStreamId = 1234;
+        size_t len = 16;
+
+        // cannot readExactly from inexistent stream
+        QCOMPARE(plugin.syncStreamReadExactly(fakeStreamId, len).size(), 0);
+
+        QVERIFY(plugin.syncLibp2pStop());
+    }
+
+    void testSyncStreamReadLp()
+    {
+        Libp2pModulePlugin plugin;
+        QVERIFY(plugin.syncLibp2pStart());
+
+        uint64_t fakeStreamId = 1234;
+        size_t maxSize = 4096;
+
+        // cannot readExactly from inexistent stream
+        QCOMPARE(plugin.syncStreamReadLp(fakeStreamId, maxSize).size(), 0);
+
+        QVERIFY(plugin.syncLibp2pStop());
+    }
+
+    void testSyncStreamWrite()
+    {
+        Libp2pModulePlugin plugin;
+        QVERIFY(plugin.syncLibp2pStart());
+
+        uint64_t fakeStreamId = 1234;
+        QByteArray data = "hello-stream";
+
+        // cannot write to inexistent stream
+        QVERIFY(!plugin.syncStreamWrite(fakeStreamId, data));
+
+        QVERIFY(plugin.syncLibp2pStop());
+    }
+
+    void testSyncStreamWriteLp()
+    {
+        Libp2pModulePlugin plugin;
+        QVERIFY(plugin.syncLibp2pStart());
+
+        uint64_t fakeStreamId = 1234;
+        QByteArray data = "hello-stream-lp";
+
+        // cannot writeLp to inexistent stream
+        QVERIFY(!plugin.syncStreamWriteLp(fakeStreamId, data));
+
+        QVERIFY(plugin.syncLibp2pStop());
+    }
+
+
+    /* ---------------------------
+     * Kademlia
      * --------------------------- */
 
     void testSyncKadGetPutValue()
