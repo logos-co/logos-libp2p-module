ea3f9ba2a98f1e426a7c44c472d82a81ed7f44a1
Gabriel Cruz <8129788+gmelodie@users.noreply.github.com>
2026-02-18 17:21:48 -0300

    feat(sync): result-like return types (#16)

--- tests/integration.cpp
+++ tests/integration.cpp
@@ -12,31 +12,31 @@ private slots:
         Libp2pModulePlugin nodeA;
         Libp2pModulePlugin nodeB;
 
-        QVERIFY(nodeA.syncLibp2pStart());
-        QVERIFY(nodeB.syncLibp2pStart());
+        QVERIFY(nodeA.syncLibp2pStart().ok);
+        QVERIFY(nodeB.syncLibp2pStart().ok);
 
-        PeerInfo nodeAPeerInfo = nodeA.syncPeerInfo();
+        PeerInfo nodeAPeerInfo = nodeA.syncPeerInfo().data.value<PeerInfo>();
 
         // cannot disconnect, invalid peerId
-        QVERIFY(!nodeB.syncDisconnectPeer("fakePeerId"));
+        QVERIFY(!nodeB.syncDisconnectPeer("fakePeerId").ok);
 
-        QVERIFY(nodeB.syncConnectPeer(nodeAPeerInfo.peerId, nodeAPeerInfo.addrs, 500));
-        QVERIFY(nodeB.syncDisconnectPeer(nodeAPeerInfo.peerId));
+        QVERIFY(nodeB.syncConnectPeer(nodeAPeerInfo.peerId, nodeAPeerInfo.addrs, 500).ok);
+        QVERIFY(nodeB.syncDisconnectPeer(nodeAPeerInfo.peerId).ok);
 
-        QVERIFY(nodeA.syncLibp2pStop());
-        QVERIFY(nodeB.syncLibp2pStop());
+        QVERIFY(nodeA.syncLibp2pStop().ok);
+        QVERIFY(nodeB.syncLibp2pStop().ok);
     }
 
     void kadPutGet()
     {
         // setup node A
         Libp2pModulePlugin nodeA;
-        QVERIFY(nodeA.syncLibp2pStart());
-        PeerInfo nodeAPeerInfo = nodeA.syncPeerInfo();
+        QVERIFY(nodeA.syncLibp2pStart().ok);
+        PeerInfo nodeAPeerInfo = nodeA.syncPeerInfo().data.value<PeerInfo>();
 
         // setup node B
         Libp2pModulePlugin nodeB({ nodeAPeerInfo });
-        QVERIFY(nodeB.syncLibp2pStart());
+        QVERIFY(nodeB.syncLibp2pStart().ok);
 
         QByteArray key = "integration-key";
         QByteArray value = "hello";
@@ -45,9 +45,9 @@ private slots:
         int quorum = 1;
         QByteArray result;
         for (int i = 0; i < 10; ++i) {
-            QVERIFY(nodeA.syncKadPutValue(key, value));
+            QVERIFY(nodeA.syncKadPutValue(key, value).ok);
             QThread::msleep(200);
-            result = nodeB.syncKadGetValue(key, quorum);
+            result = nodeB.syncKadGetValue(key, quorum).data.value<QByteArray>();
             if (!result.isEmpty())
                 break;
 
@@ -56,10 +56,10 @@ private slots:
 
         QCOMPARE(result, value);
 
-        QVERIFY(nodeB.syncDisconnectPeer(nodeAPeerInfo.peerId));
+        QVERIFY(nodeB.syncDisconnectPeer(nodeAPeerInfo.peerId).ok);
 
-        QVERIFY(nodeA.syncLibp2pStop());
-        QVERIFY(nodeB.syncLibp2pStop());
+        QVERIFY(nodeA.syncLibp2pStop().ok);
+        QVERIFY(nodeB.syncLibp2pStop().ok);
     }
 };
 
