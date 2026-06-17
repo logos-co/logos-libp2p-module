51a62586ed71bdcf3b4ad8bcc21184508e5cbf7c
Gabriel Cruz <gabe@gmelodie.com>
2026-06-17 12:00:13 -0300

    chore: use openmetrics fix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     libp2p.url = "github:vacp2p/nim-libp2p";
 
     openmetrics-module = {
-      url = "github:logos-co/openmetrics-module";
+      url = "github:logos-co/openmetrics-module/fix/ipc-main-thread";
       inputs.logos-module-builder.follows = "logos-module-builder";
     };
 
