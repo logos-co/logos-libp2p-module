c2f07236ed538fad1ac5ff5c7d0f585612134d5c
Gabriel Cruz <gabe@gmelodie.com>
2026-06-17 14:35:07 -0300

    chore(nix): bump deps

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
-    libp2p.url = "github:vacp2p/nim-libp2p";
+    libp2p.url = "github:vacp2p/nim-libp2p/fix/ci/unused-chronicles";
 
     openmetrics-module = {
       url = "github:logos-co/openmetrics-module";
