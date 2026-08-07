3330f761448f101e12fc83431ad7a9b2e1b483c5
Gabriel Cruz <gabe@gmelodie.com>
2026-08-07 15:03:10 -0300

    chore: point back to master

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
-    libp2p.url = "github:vacp2p/nim-libp2p/fix/cbind/advertise-full-xpr";
+    libp2p.url = "github:vacp2p/nim-libp2p/master";
 
     openmetrics-module = {
       url = "github:logos-co/openmetrics-module";
